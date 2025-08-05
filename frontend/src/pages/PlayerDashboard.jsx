26ce9bb70b310fc52287e62519df780bf54eb112
thealmight <thealmigtforever@gmail.com>
2025-08-05 20:40:03 +0530

    fe

--- frontend/src/pages/PlayerDashboard.jsx
+++ frontend/src/pages/PlayerDashboard.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useGame } from '../context/GameContext';
 import useCountdown from '../hooks/useCountdown';
-// import { socket } from '../socket'; 
+import { socket } from '../socket'; 
 import { io } from 'socket.io-client';
 
 export default function PlayerDashboard() {
